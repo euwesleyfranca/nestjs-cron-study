2c8e97e46f046b1773f803780c0301c931fb7976
wesleyfranca <we.prog@hotmail.com>
2021-11-29 13:37:38 -0300

    dinamic cron

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TaskModule } from './task/task.module';
-import { NotificationModule } from './notification/notification.module';
-
 @Module({
-  imports: [ScheduleModule.forRoot(), TaskModule, NotificationModule],
+  imports: [ScheduleModule.forRoot(), TaskModule],
   controllers: [],
   providers: [],
 })
